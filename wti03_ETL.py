71a26d226d25946eb9f22122aa2aae2cea48832e
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-05-31 08:37:52 +0200

    chore(wti-03): change dict names

--- wti03_ETL.py
+++ wti03_ETL.py
@@ -3,13 +3,13 @@ import pandas
 
 def build_dataframe(user_rates_with_movies):
     genres_list = list(set([item for sublist in user_rates_with_movies['genres'].tolist() for item in sublist]))
-    json_columns_list = list(['userID', 'movieID', 'rating'])
-    json_columns_list.extend(list(('genre-' + item) for item in genres_list))
+    json_columns_list = list(['user_id', 'movie_id', 'rating'])
+    json_columns_list.extend(list(('genre_' + item).lower().replace('-', '_') for item in genres_list))
     elements = {}
     iterator = 0
 
-    for row in user_rates_with_movies[['userID', 'movieID', 'rating', 'genres']].to_dict(orient='records'):
-        element = [row['userID'], row['movieID'], row['rating']]
+    for row in user_rates_with_movies[['user_id', 'movie_id', 'rating', 'genres']].to_dict(orient='records'):
+        element = [row['user_id'], row['movie_id'], row['rating']]
 
         for genre in genres_list:
             element.extend([int((genre in row['genres']) if 1 else 0)])
@@ -24,7 +24,7 @@ def count_avg_per_user(ratings, user):
     element = {}
 
     for col in ratings:
-        if col in ['movieID', 'userID', 'rating']:
+        if col in ['movie_id', 'user_id', 'rating']:
             continue
         element[col] = 0.1
     return element
@@ -34,7 +34,7 @@ def count_avg(ratings):
     element = {}
 
     for col in ratings:
-        if col in ['movieID', 'userID', 'rating']:
+        if col in ['movie_id', 'user_id', 'rating']:
             continue
         element[col] = 0.2
     return element
