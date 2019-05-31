2c92611c17009a5b6359983866a0e1e748d6bf17
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-05-31 08:37:13 +0200

    chore(wti-04): change dict names

--- wti04_module.py
+++ wti04_module.py
@@ -5,9 +5,9 @@ import wtiproj01_module
 
 def join_tables(user_ratedmovies, movie_genres):
     user_rates_with_movies = wtiproj01_module.join_tables(user_ratedmovies, movie_genres)
-    genres_list = list(set([item for sublist in user_rates_with_movies['genres'].tolist() for item in sublist]))
-    json_columns_list = list(['userID', 'movieID', 'rating'])
-    json_columns_list.extend(list(('genre-' + item) for item in genres_list))
+    genres_list = list(set([item.lower().replace('-', '_') for sublist in user_rates_with_movies['genres'].tolist() for item in sublist]))
+    json_columns_list = list(['user_id', 'movie_id', 'rating'])
+    json_columns_list.extend(list(('genre-' + item).lower().replace('-', '_') for item in genres_list))
     elements = {}
     iterator = 0
 
@@ -32,7 +32,7 @@ def build_dict_from_dataframe(dataframe):
 
 
 def avg_movies_by_genre(dataframe, genre):
-    avg = pandas.pivot_table(dataframe, values='rating', aggfunc=numpy.average, index=['genre-' + genre])
+    avg = pandas.pivot_table(dataframe, values='rating', aggfunc=numpy.average, index=['genre_' + genre])
     avg = avg.drop(0)
 
     if avg.empty:
@@ -45,7 +45,7 @@ def avg_movie_by_user_and_genre(dataframe, user_id, genre):
     return {
         'user_id': user_id,
         'genre': genre,
-        'rating': avg_movies_by_genre(dataframe.loc[lambda df: df.userID == user_id, :], genre)['rating']
+        'rating': avg_movies_by_genre(dataframe.loc[lambda df: df.user_id == user_id, :], genre)['rating']
     }
 
 
