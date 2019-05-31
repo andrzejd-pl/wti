42ecaa150611536cdbecd558f1bf42918571ed56
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-05-31 08:39:31 +0200

    chore(wti-05): add client API

---
+++ wti05_api_client.py
@@ -0,0 +1,130 @@
+import json
+
+import requests
+
+import wti03_ETL
+
+
+def fuction():
+    r = requests.get('http://localhost:8888/ratings')
+    wti03_ETL.print_request(r)
+
+    r = requests.post('http://localhost:8888/rating', json.dumps({
+        "user_id": 75,
+        "movie_id": 110,
+        "rating": 4,
+        "genre_romance": 0,
+        "genre_drama": 1,
+        "genre_mystery": 0,
+        "genre_sci_fi": 0,
+        "genre_short": 0,
+        "genre_war": 1,
+        "genre_animation": 0,
+        "genre_musical": 0,
+        "genre_documentary": 0,
+        "genre_crime": 0,
+        "genre_children": 0,
+        "genre_thriller": 0,
+        "genre_horror": 0,
+        "genre_film_noir": 0,
+        "genre_western": 0,
+        "genre_imax": 0,
+        "genre_adventure": 0,
+        "genre_fantasy": 0,
+        "genre_action": 1,
+        "genre_comedy": 0
+    }))
+    wti03_ETL.print_request(r)
+
+    r = requests.post('http://localhost:8888/rating', json.dumps({
+        "user_id": 75,
+        "movie_id": 112,
+        "rating": 5,
+        "genre_romance": 0,
+        "genre_drama": 1,
+        "genre_mystery": 0,
+        "genre_sci_fi": 0,
+        "genre_short": 0,
+        "genre_war": 1,
+        "genre_animation": 0,
+        "genre_musical": 0,
+        "genre_documentary": 0,
+        "genre_crime": 0,
+        "genre_children": 0,
+        "genre_thriller": 0,
+        "genre_horror": 0,
+        "genre_film_noir": 0,
+        "genre_western": 0,
+        "genre_imax": 0,
+        "genre_adventure": 0,
+        "genre_fantasy": 0,
+        "genre_action": 1,
+        "genre_comedy": 0
+    }))
+    wti03_ETL.print_request(r)
+    #
+    # r = requests.post('http://localhost:8888/rating', json.dumps({
+    #     "user_id": 73,
+    #     "movie_id": 110,
+    #     "rating": 6,
+    #     "genre_romance": 0,
+    #     "genre_drama": 1,
+    #     "genre_mystery": 0,
+    #     "genre_sci_fi": 0,
+    #     "genre_short": 0,
+    #     "genre_war": 1,
+    #     "genre_animation": 0,
+    #     "genre_musical": 0,
+    #     "genre_documentary": 0,
+    #     "genre_crime": 0,
+    #     "genre_children": 0,
+    #     "genre_thriller": 0,
+    #     "genre_horror": 0,
+    #     "genre_film_noir": 0,
+    #     "genre_western": 0,
+    #     "genre_imax": 0,
+    #     "genre_adventure": 0,
+    #     "genre_fantasy": 0,
+    #     "genre_action": 1,
+    #     "genre_comedy": 0
+    # }))
+    # wti03_ETL.print_request(r)
+    #
+    # r = requests.post('http://localhost:8888/rating', json.dumps({
+    #     "user_id": 73,
+    #     "movie_id": 113,
+    #     "rating": 1,
+    #     "genre_romance": 0,
+    #     "genre_drama": 1,
+    #     "genre_mystery": 0,
+    #     "genre_sci_fi": 0,
+    #     "genre_short": 0,
+    #     "genre_war": 1,
+    #     "genre_animation": 0,
+    #     "genre_musical": 0,
+    #     "genre_documentary": 0,
+    #     "genre_crime": 0,
+    #     "genre_children": 0,
+    #     "genre_thriller": 0,
+    #     "genre_horror": 0,
+    #     "genre_film_noir": 0,
+    #     "genre_western": 0,
+    #     "genre_imax": 0,
+    #     "genre_adventure": 0,
+    #     "genre_fantasy": 0,
+    #     "genre_action": 1,
+    #     "genre_comedy": 0
+    # }))
+    # wti03_ETL.print_request(r)
+
+    r = requests.get('http://localhost:8888/avg-genre-ratings/all-users')
+    wti03_ETL.print_request(r)
+
+    r = requests.get('http://localhost:8888/avg-genre-ratings/75')
+    wti03_ETL.print_request(r)
+
+    r = requests.delete('http://localhost:8888/ratings')
+    wti03_ETL.print_request(r)
+
+
+fuction()
