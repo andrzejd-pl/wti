a8b0303291e7ca22b6f127ffc5bf4abe1b22d6c2
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-06-01 20:12:05 +0200

    chore(wti-06): standardise main file^

--- wti06_main.py
+++ wti06_main.py
@@ -1,7 +1,13 @@
 from wti06_API import APILogic
 from wti05_module import FlaskAppWrapper
 
-app = FlaskAppWrapper('API - Logic')
-api_logic = APILogic(app)
-api_logic.add_all_endpoints()
-app.run()
+
+def main():
+    app = FlaskAppWrapper('API - Logic')
+    api_logic = APILogic(app)
+    api_logic.add_all_endpoints()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
