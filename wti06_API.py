eb81313704a3c5866710c19b7d45e2ac7fc493ac
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-06-01 20:05:02 +0200

    chore(wti-06): modify wti-06 api class

--- wti06_API.py
+++ wti06_API.py
@@ -2,50 +2,44 @@ import json
 
 import flask
 from flask import Response
-from cassandra.cluster import Cluster
-from cassandra.query import dict_factory
 
 import wti06_ETL
 from wti05_module import FlaskAppWrapper
-from wti06_cassandra_client import create_keyspace, create_table, push_data_table, get_data_table, clear_table, get_data_table_per_column_value
+from wti06_cassandra_client import CassandraClient
 
 
 class APILogic(object):
-    def __init__(self, app=FlaskAppWrapper(name='tmp2')):
+    def __init__(self, app=FlaskAppWrapper(name='wti-06')):
         self.app = app
-        self.keyspace = "user_ratings"
         self.table = "user_avg_rating"
-        self.cluster = Cluster(['127.0.0.1'], port=9042)
-        self.session = self.cluster.connect()
-        create_keyspace(self.session, self.keyspace)
-        self.session.set_keyspace(self.keyspace)
-        self.session.row_factory = dict_factory
-        create_table(self.session, self.keyspace, self.table)
+        self.client = CassandraClient()
+        self.client.create_table(self.table)
+        self.client.clear_table(self.table)
         self.next_index = 0
 
     def add_row(self):
         data = json.loads(flask.request.data)
         data['user_ratings_id'] = self.next_index
         self.next_index += 1
-        push_data_table(self.session, self.keyspace, self.table, data)
+        self.client.push_data_table(self.table, data)
         return Response(status=201)
 
     def get_all(self):
         all_rows = []
-        for x in get_data_table(self.session, self.keyspace, self.table):
+        for x in self.client.get_data_table(self.table):
             all_rows.append(dict(x))
 
         return Response(status=200, response=json.dumps(all_rows),
                         mimetype='application/json')
 
     def delete(self):
-        clear_table(self.session, self.keyspace, self.table)
+        self.client.clear_table(self.table)
 
         return Response(status=204)
 
     def get_all_avg_users(self):
         all_rows = []
-        for x in get_data_table(self.session, self.keyspace, self.table):
+        for x in self.client.get_data_table(self.table):
             all_rows.append(dict(x))
 
         return flask.Response(response=json.dumps(wti06_ETL.count_avg(all_rows)), status=201,
@@ -53,7 +47,7 @@ class APILogic(object):
 
     def get_avg_user(self, user):
         all_rows = []
-        for x in get_data_table_per_column_value(self.session, self.keyspace, self.table, 'user_id', user):
+        for x in self.client.get_data_table_per_column_value(self.table, 'user_id', user):
             all_rows.append(dict(x))
 
         return flask.Response(response=json.dumps(wti06_ETL.count_avg(all_rows)), status=201,
